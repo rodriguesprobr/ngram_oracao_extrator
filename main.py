bc5815db7fa8ba9fb2697ae348618e6a32c9b1c6
Fernando de Assis Rodrigues <fernando@rodrigues.pro.br>
2024-05-24 12:51:34 -0300

    Início do tratamento com spacy e criação dos ngrams (um a três). Troca do python de 3.9 para 3.10 (venv).

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import fnmatch
 import os
 from datetime import datetime
 
+import spacy
+from spacy_ngram import NgramComponent
+
 comunicacoes_cientificas_dir = "data/comunicacoes_cientificas"
 comunicacoes_cientificas_csv = "data/dados_tratados.csv"
 remocoes = ["\t", "\r", "\n", "  "]
@@ -18,6 +21,8 @@ def limpar(texto):
     return texto.strip().lower()
 
 
+nlp = spacy.load('pt_core_news_lg')
+nlp.add_pipe('spacy-ngram')
 dados_tratados = open(file=comunicacoes_cientificas_csv, mode="w", encoding="utf-8")
 dados_tratados.write("ANO\tARQUIVO\tCORPUS\n")
 for ano in os.listdir(comunicacoes_cientificas_dir):
@@ -28,4 +33,10 @@ for ano in os.listdir(comunicacoes_cientificas_dir):
         if fnmatch.fnmatch(comunicacao_cientifica, '*.txt'):
             conteudo = limpar(open(os.path.join(comunicacoes_cientificas_dir, ano, comunicacao_cientifica), "r", encoding="UTF-8").read())
             dados_tratados.write(f"{ano}\t{comunicacao_cientifica}\t{conteudo}\n")
+            doc = nlp(conteudo)
+            unigram = doc._.ngram_1
+            bigram = doc._.ngram_2
+            trigram = doc._.ngram_3
+
+
 dados_tratados.close()
