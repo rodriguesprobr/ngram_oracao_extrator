e465d87a83df555467c0aa26e1d2c8c0b65a2e0f
Fernando de Assis Rodrigues <fernando@rodrigues.pro.br>
2024-06-18 12:25:35 -0300

    Ajustes no código para criar diretório util para scripts. Ajuste no nome para script principal chamar processar.py.

--- util/thread.py
+++ util/thread.py
@@ -1,16 +1,17 @@
 import os
 
-import spacy
+import spacy, spacy_ngram
 
-import util
-from preprocessamento import PreProcessamento
+from util.config import config
+from util.log import log
+from util.preprocessamento import PreProcessamento
 from util.sql import ngram_inserir, comunicacao_cientifica_ngram_inserir, comunicacao_cientifica_analisar, ano_selecionar_um, \
     comunicacao_cientifica_analisar_finalizar
 
 
 def processar_comunicacao_cientifica(comunicacao_cientifica):
     ano = ano_selecionar_um(comunicacao_cientifica.ano_id)
-    util.log(f"Processando - Ano {ano.ano} - Comunicação Científica {comunicacao_cientifica.arquivo_nome}")
+    log(f"Processando - Ano {ano.ano} - Comunicação Científica {comunicacao_cientifica.arquivo_nome}")
     nlp = spacy.load('pt_core_news_lg')
     nlp.add_pipe('spacy-ngram', config={'ngrams': (1, 2, 3)})
     preprocessor = PreProcessamento(
@@ -23,7 +24,7 @@ def processar_comunicacao_cientifica(comunicacao_cientifica):
     )
     conteudo = preprocessor.preprocess_text(
         open(
-            os.path.join(util.get_config("comunicacoes_cientificas_dir"), str(ano.ano), comunicacao_cientifica.arquivo_nome),
+            os.path.join(config("comunicacoes_cientificas_dir"), str(ano.ano), comunicacao_cientifica.arquivo_nome),
             "r",
             encoding="UTF-8"
         ).read()
@@ -42,7 +43,7 @@ def processar_comunicacao_cientifica(comunicacao_cientifica):
         trigram_id = ngram_inserir(trigram, 3)
         comunicacao_cientifica_ngram_inserir(comunicacao_cientifica.id, trigram_id)
     comunicacao_cientifica_analisar_finalizar(comunicacao_cientifica.id)
-    util.log(f"Término - Ano {ano.ano} - Comunicação Científica {comunicacao_cientifica.arquivo_nome}")
+    log(f"Término - Ano {ano.ano} - Comunicação Científica {comunicacao_cientifica.arquivo_nome}")
     comunicacao_cientifica_thread = comunicacao_cientifica_analisar()
     if comunicacao_cientifica_thread is not False:
         processar_comunicacao_cientifica(comunicacao_cientifica_thread)
