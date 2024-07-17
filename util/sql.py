4c0021410625840e09d540d04692589f6fadd73a
Fernando de Assis Rodrigues <fernando@rodrigues.pro.br>
2024-07-17 09:48:15 -0300

    Troca de nome de atributo de counter para contador em comunicacao_cientifica_ngram.

--- util/sql.py
+++ util/sql.py
@@ -31,7 +31,7 @@ ComunicacaoCientificaNgram = db.Table(
     metadata,
     db.Column("comunicacao_cientifica_id", db.Integer, primary_key=True, foreign_key="comunicacao_cientifica.id"),
     db.Column("ngram_id", db.Integer, primary_key=True, foreign_key="ngram.id"),
-    db.Column("counter", db.Integer, nullable=False)
+    db.Column("contador", db.Integer, nullable=False)
 )
 metadata.create_all(engine)
 
@@ -132,11 +132,11 @@ def ngram_por_ano_selecionar(tipo):
         if tipo == "ocorrencia":
             sql += f"count(case when ano = {ano.ano} then 1 end) as '{ano.ano}',"
         elif tipo == "frequencia":
-            sql += f"sum(case when ano = {ano.ano} then counter end) as '{ano.ano}',"
+            sql += f"sum(case when ano = {ano.ano} then contador end) as '{ano.ano}',"
     if tipo == "ocorrencia":
         sql += f"count(ccn.ngram_id) as total"
     elif tipo == "frequencia":
-        sql += f"sum(ccn.counter) as total"
+        sql += f"sum(ccn.contador) as total"
     sql += """
         FROM ngram n
         LEFT JOIN comunicacao_cientifica_ngram ccn ON ccn.ngram_id = n.id
@@ -200,12 +200,12 @@ def comunicacao_cientifica_ngram_inserir(comunicacao_cientifica_id, ngram_id):
         result = conn.execute(
             db.insert(ComunicacaoCientificaNgram).values(comunicacao_cientifica_id=comunicacao_cientifica_id,
                                                          ngram_id=ngram_id,
-                                                         counter=1)).lastrowid
+                                                         contador=1)).lastrowid
         # conn.commit()
     else:
         result = conn.execute(db.update(ComunicacaoCientificaNgram).where(
             ComunicacaoCientificaNgram.c.comunicacao_cientifica_id == comunicacao_cientifica_id
         ).where(
             ComunicacaoCientificaNgram.c.ngram_id == ngram_id
-        ).values(counter=ComunicacaoCientificaNgram.c.counter+1)).lastrowid
+        ).values(contador=ComunicacaoCientificaNgram.c.contador+1)).lastrowid
     return result
