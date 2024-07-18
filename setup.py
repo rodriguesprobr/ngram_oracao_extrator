68387795dac1d5394ad364944d50d5804f8d0daa
Fernando de Assis Rodrigues <fernando@rodrigues.pro.br>
2024-07-18 11:44:42 -0300

    Inserção do setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ngram_oracao_extrator',
+    version='1.1',
+    packages=['SQLAlchemy', 'spacy', 'tqdm'],
+    url='https://github.com/rodriguesprobr/ngram_oracao_extrator',
+    license='Creative Commons 4.0 BY-NC-SA',
+    author='RODRIGUES, F. A.; FARINELLI, F.; ARAKAKI, A. C. S.; ARAKAKI, F. A.',
+    author_email='deassis@ufpa.br',
+    description='Versão alpha do Projeto de Extração de n-grams e Isolamento de Orações de Textos'
+)
